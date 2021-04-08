d2b2c8dcbb8971565a9ca74ebb2d670fdf3f8cbc
victor-singha <vicunite2@gmail.com>
2021-04-08 11:59:36 +0530

    headcount and tailcount added

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
-result=$((RANDOM%2))
-if [ $result -eq 0 ]
-then
-	echo "head"
-else
-	echo "tail"
-fi
+headcount=0
+tailcount=0
+for ((i=1;i<50;i++))
+do
+	result=$((RANDOM%2))
+	if [ $result -eq 0 ]
+	then
+		headcount=$((headcount+1))
+	else
+		tailcount=$((tailcount+1))
+	fi
+done
+echo "heads: " $headcount
+echo "tails: " $tailcount
